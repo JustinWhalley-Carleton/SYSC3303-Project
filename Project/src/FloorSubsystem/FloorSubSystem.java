05de1a7f718883a154ef01bbf1901c6e24daf5a6
keith <hgyixg@gmail.com>
2021-02-04 19:48:00 -0500

    modifying floor
    Not finished yet!

--- Project/src/FloorSubsystem/FloorSubSystem.java
+++ Project/src/FloorSubsystem/FloorSubSystem.java
@@ -1,17 +1,22 @@
 package FloorSubsystem;
 
-public class FloorSubSystem implements Runnable{
+import java.time.LocalTime;
+
+public class FloorSubSystem implements Runnable {
     // Constants
     private final int MIN_FLOOR;
     private final int MAX_FLOOR;
     private byte[] requestedDir;
     private Floor[] floors;
 
+    //    private SchedulerSubsystem scheduler;
     public FileLoader instructionFile;
+    // current Time of the system
+    public LocalTime curTime;
 
-    public FloorSubSystem(int maxFloor) throws Exception{
+    public FloorSubSystem(int maxFloor) throws Exception {
         // Error checking
-        if (maxFloor <= 1){
+        if (maxFloor <= 1) {
             throw new Exception("incompatible setting: maxFloor should be higher than 2.");
         }
 
@@ -19,7 +24,7 @@ public class FloorSubSystem implements Runnable{
         this.MIN_FLOOR = 1;
         this.MAX_FLOOR = maxFloor;
         floors = new Floor[MAX_FLOOR];
-        for (int f = 1; f <= maxFloor; ++ maxFloor){
+        for (int f = 1; f <= maxFloor; ++maxFloor) {
             floors[f] = new Floor(f, f == MIN_FLOOR, f == MAX_FLOOR);
         }
 
@@ -27,32 +32,68 @@ public class FloorSubSystem implements Runnable{
         instructionFile = new FileLoader();
     }
 
-    public void run(){
+    public void run() {
+        nextInstruction();
+        boolean instructionSent = false;
 
+        // init current time based on time set on first instruction
+        curTime = instructionFile.getTime();
+
+        while (true) {
+            // send instruction if needed
+            if (instructionSent) {
+                // read instruction
+                nextInstruction();
+            } else {
+                // compare time stamp
+                if (curTime.isAfter(instructionFile.getTime())) {
+                    // send it now
+                    send(instructionFile.toString());
+                    instructionSent = true;
+                }
+            }
+
+            // receive instruction if needed
+            receive();
+        }
     }
 
-    public void selectFloor(byte[] in){
+
+    // Read next instruction from file
+    public void nextInstruction() {
+        try {
+            instructionFile.nextLine();
+        } catch (Exception e) {
+            System.out.println("ERROR: Read instruction file failed.");
+        }
+    }
+
+    public void selectFloor(byte[] in) {
 
     }
 
     // read command from a file
-    public void readCommand(String str){
+    public void readCommand(String str) {
 
     }
 
-    public byte[] getInfo(){
+    public byte[] getInfo() {
         return null;
     }
 
 
-    // send method called by scheduler: return byte[] to scheduler.
-    public byte[] send(){
-        return null;
+    // send method: send data to scheduler.
+    public void send(String data) {
+
+//        scheduler.AddRequest(Integer(1), data);
     }
 
-    // receive method called by scheduler: save message from scheduler to var.
-    public void receive(byte[] message){
+    // receive method: save message from scheduler.
+    public byte[] receive() {
         // process message from scheduler
+
+//        System.out.println(scheduler.checkRequest(Integer(1));
+        return null;
     }
 
 }
