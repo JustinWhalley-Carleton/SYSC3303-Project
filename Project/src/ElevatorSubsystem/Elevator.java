adb47b98511035ce47085b50ccd0551b9395bc19
Gurjit Gill <gurjitgill@cmail.carleton.ca>
2021-02-04 20:48:55 -0500

    Updated the send() and recieve()

--- Project/src/ElevatorSubsystem/Elevator.java
+++ Project/src/ElevatorSubsystem/Elevator.java
@@ -1,3 +1,6 @@
+package ElevatorSubsystem;
+import Scheduler.Scheduler;
+
 /**
  * 
  */
@@ -11,10 +14,14 @@ public class Elevator implements Runnable{
 	public int curFloor;
 	private int floorSelection;
 	private boolean doorStatus;
+	
+	private Scheduler scheduler;
 
-	public Elevator() {
+	public Elevator(Scheduler scheduler) {
 		this.curFloor = 0;
 		this.doorStatus = true;
+		
+		scheduler = new Scheduler();
 	}
 	
 	public void selectFloor(int floor) {
@@ -25,12 +32,12 @@ public class Elevator implements Runnable{
 		return null;
 	}
 	
-	public byte[] send() {
-		return null;
+	public void send() {
+		scheduler.elevatorAddRequest(1, "Going up");
 	}
 	
 	public void recieve(byte[] info) {
-		
+		scheduler.elevatorCheckRequest(floorSelection);
 	}
 	
 	private void openDoor() {
