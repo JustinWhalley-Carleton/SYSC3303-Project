49ff317fce58456ce625d020d4ab74197e12ab5b
Justin <justinwhalley@cmail.carleton.ca>
2021-02-26 23:11:14 -0500

    fixed errors

--- Project/src/ElevatorSubsystem/Elevator.java
+++ Project/src/ElevatorSubsystem/Elevator.java
@@ -1,5 +1,6 @@
 package ElevatorSubsystem;
 
+import java.util.ArrayList;
 
 /**
  * @author Gill
@@ -8,16 +9,43 @@ package ElevatorSubsystem;
 public class Elevator {
 	
 	public int curFloor;
-	private int[] destFloors;
+	private ArrayList<Integer> destFloors;
 	private boolean doorStatus;
 
 	public Elevator(int curFloor, boolean doorStatus) {
-		this.curFloor = 0;
-		this.doorStatus = true;
+		this.curFloor = curFloor;
+		this.doorStatus = doorStatus;
+		destFloors = new ArrayList<Integer>();
 	}
 	
 	public void selectFloor(int[] floors) {
-		this.destFloors = floors;
+		for(int i : floors) {
+			destFloors.add((Integer)i);
+		}
+	}
+	
+	public void addDest(int floor) {
+		destFloors.add((Integer)floor);
+		if(curFloor > floor) {
+			System.out.println("GOING DOWN");
+		} else if (floor > curFloor) {
+			System.out.println("GOING UP");
+		} else {
+			removeFloor(floor);
+			return;
+		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("IN IDLE");
+		removeFloor(floor);
+	}
+	
+	public void removeFloor(int floor) {
+		destFloors.remove((Integer)floor);
 	}
 	
 	public byte[] getInfo() {
