7a2d5a99f3d30d788fc7666a42eb9566eaa5fca1
Yisheng Li <yisheng.li@carleton.ca>
2021-03-12 17:20:55 -0500

    Update Scheduler.java

--- Project/src/Scheduler/Scheduler.java
+++ Project/src/Scheduler/Scheduler.java
@@ -13,12 +13,14 @@ import common.*;
 
 public class Scheduler implements Runnable {
 
+	private final byte[] CheckMSG = Common.encodeConfirmation(Common.CONFIRMATION.CHECK);
+
 	private int inState = 0; // 0 is wait; 1 is sending; -1 is receiving  
 	private ElevtState[] elevtStates; // also is the state of the scheduler
 	private FloorState[] floorStates;
 	private Queue<byte[]>   msgFromFloorSub, msgToElevtSub, msgToFloorSub;
 
-	public RPC rpcFloor, rpcElevt;
+	private RPC rpcFloor, rpcElevt;
 
 	/**
 	 * Constructor
@@ -42,33 +44,28 @@ public class Scheduler implements Runnable {
 	}
 
 
-
-	// sub send:
-
     /**
-	 * update
+	 *
 	 *
      * @param msg The message sent by the elevator subsystem.
      */
-    public void elevtSubAddMsg (byte[] msg) {
-    	this.inState = -1;
-    	int[] message = Common.decode(msg);
+	private void elevtSubAddMsg (byte[] msg) {
+		int[] message = Common.decode(msg);
 		
-    	System.out.println("Scheduler got message from elevtSub: " + Arrays.toString(message));
-	
-		
+    	//System.out.println("Scheduler got message from elevtSub: " + Arrays.toString(message));
 		int elevt = message[0];
 		int floor = message[1];
 		int dir = message[2];
 		int dest = message[3];
 
+		// update elevt States
 		elevtStates[elevt-1].setFloor(floor);
 		elevtStates[elevt-1].setDir(dir);
 		elevtStates[elevt-1].setDest(dest);
 
-
 		//if the the elevator stops on a floor, dismiss floor buttons
 		if (dir == 0){
+			//TO-DO: find which (uyp/down) button to dismiss
 			if (floor != 1) {
 				byte[] oneMsgToFloorSub = Common.encodeScheduler(1, floor,0);
 				System.out.println("Scheduler sent message to FloorSub: " +  Arrays.toString(Common.decode(oneMsgToFloorSub)) + " @ time = " + LocalTime.now());
@@ -79,10 +76,10 @@ public class Scheduler implements Runnable {
 				System.out.println("Scheduler sent message to FloorSub: " +  Arrays.toString(Common.decode(oneMsgToFloorSub)) + " @ time = " + LocalTime.now());
 				msgToFloorSub.offer(oneMsgToFloorSub);
 			}
+
+
 		}
 
-    	updateSchedule();
-    	this.inState = 0;
         return;
     }
 
@@ -90,100 +87,102 @@ public class Scheduler implements Runnable {
 	 *
 	 * @param msg The message sent by the floor subsystem.
 	 */
-	public void floorSubAddMsg (byte[] msg) {
-		this.inState = -1;
-
-
-
-
+	private void floorSubAddMsg (byte[] msg) {
 		int[] message = Common.decode(msg);
-		System.out.println("Scheduler got message from floor sub: " + Arrays.toString(message)  + " @ time = " + LocalTime.now());
+		//System.out.println("Scheduler got message from floor sub: " + Arrays.toString(message)  + " @ time = " + LocalTime.now());
 		int floor = message[0];
 		int dir = message[1];
 
 
-		byte[] oneMsgToElevtSub = Common.encodeScheduler(1, floor,0);
-		System.out.println("Scheduler sent message to ElevtSub: " +  Arrays.toString(Common.decode(oneMsgToElevtSub)) + " @ time = " + LocalTime.now());
-
+		//System.out.println("Scheduler sent message to ElevtSub: " +  Arrays.toString(Common.decode(oneMsgToElevtSub)) + " @ time = " + LocalTime.now());
+		//msgFromFloorSub.offer(oneMsgFromFloorSub);
+		int closestElevt = findClosestElevt(floor,dir);
+		
+		byte[] oneMsgToElevtSub = Common.encodeScheduler(closestElevt, floor,dir);
 		msgToElevtSub.offer(oneMsgToElevtSub);
-
-		updateSchedule();
-		this.inState = 0;
 		return;
 	}
 
 
-
-
-    // sub get:
-    /**
-     * 
-     * @return a message to the elevator subsystem
-     */
-    public byte[] elevtSubCheckMsg() {
-    	this.inState = 1;
-    	byte[] msg = msgToElevtSub.poll();
-    	this.inState = 0;
-    	return msg;
-	}
-
-
-	/**
-	 *
-	 * @return message to the floor sub system
-	 */
-	public byte[] floorSubCheckMsg() {
-    	this.inState = 1;
-    	byte[] msg = msgToFloorSub.poll();
-    	this.inState = 0;
-
-		return msg;
-	}
-
-
-
-    /**
-	 * Update elevtStates a msgToElevtSub Schedule based on all data
-	 *
-	 */
-	private void updateSchedule() {
-
-	}
-	/**
-	 * Recevice massgaes from FloorSub and ElevtSub
-	 */
-	private void receive() {
-		byte[] message1 = rpcFloor.receivePacket();
-		if (message1 != null){
-			floorSubAddMsg(message1);
-		}
-
-		byte[] message2 = rpcElevt.receivePacket();
-		if (message2 != null){
-			elevtSubAddMsg(message2);
-		}
-		return;
-	}
 
 	/**
 	 * Send massgaes to FloorSub and ElevtSub if there is any
 	 */
-	private void send(){
-		byte[] msg = floorSubCheckMsg();
-		if ( msg != null) {
-			rpcFloor.sendPacket(msg);
+	private void sendReceiveFloorSub() {
+		// send to FloorSub
+		this.inState = 1;
+		byte[] msgSend = msgToFloorSub.poll();
+		if (msgSend != null) {
+			rpcFloor.sendPacket(msgSend);
+		} else {
+			rpcFloor.sendPacket(CheckMSG);
 		}
-		msg = elevtSubCheckMsg();
-		if ( msg != null) {
-			rpcElevt.sendPacket(msg);
+		this.inState = 0;
+
+		// receive from FloorSub
+		this.inState = -1;
+		byte[] msgReceive = rpcFloor.receivePacket();
+		if (Common.findType(msgReceive) != Common.TYPE.CONFIRMATION) {
+			floorSubAddMsg(msgReceive);
 		}
+		this.inState = 0;
 	}
+
+	private void sendReceiveElevtSub(){
+		// check ElevtSub
+		this.inState = 1;
+		byte[] msgSend = msgToElevtSub.poll();
+		if ( msgSend != null) {
+			rpcElevt.sendPacket(msgSend);
+		} else{
+			rpcElevt.sendPacket(CheckMSG);
+		}
+		this.inState = 0;
+		// receive from ElevtSub
+
+		this.inState = -1;
+		byte[] msgReceive = rpcElevt.receivePacket();
+		if (Common.findType(msgReceive) != Common.TYPE.CONFIRMATION){
+			elevtSubAddMsg(msgReceive);
+		}
+		this.inState = 0;
+
+	}
+
+
+	/**
+	 * Update elevtStates a msgToElevtSub Schedule based on all data
+	 *
+	 */
+	private int findClosestElevt(int floor, int dir) {
+		int result = 0;
+
+		return result;
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 	@Override
 	public void run() {
+
 		while (true) {
-			receive();
-			send();
+			try {
+				sendReceiveFloorSub();
+				this.wait(200);
+				sendReceiveElevtSub();
+				this.wait(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
