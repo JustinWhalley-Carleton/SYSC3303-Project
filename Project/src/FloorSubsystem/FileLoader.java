6bc30666fdccefbe46bf3741d8b56434c53a5cdf
Gurjit Gill <gurjitgill@cmail.carleton.ca>
2021-04-02 11:31:35 -0400

    Implemented Elevator Timing
    Can now also read the elevator errors within the same text file

--- Project/src/FloorSubsystem/FileLoader.java
+++ Project/src/FloorSubsystem/FileLoader.java
@@ -9,214 +9,223 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileLoader {
-    private File instructionFile = new File("src/test/testFile.txt");
-    private Scanner scanner;
+	private File instructionFile = new File("src/test/testFile.txt");
+	private Scanner scanner;
 
-    private boolean endOfFile;
+	private boolean endOfFile;
 
-    private String curLine;
-    private String[] lineSplit;
+	private String curLine;
+	private String[] lineSplit;
 
-    // Saves destination floors for each departure floor
-    HashMap<Integer, ArrayList<Integer>> destinations;
+	// Saves destination floors for each departure floor
+	HashMap<Integer, ArrayList<Integer>> destinations;
 
-    public FileLoader() throws Exception{
-        try {
-            scanner = new Scanner(instructionFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            endOfFile = true;
-        }
-        endOfFile = false;
+	public FileLoader() throws Exception{
+		try {
+			scanner = new Scanner(instructionFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			endOfFile = true;
+		}
+		endOfFile = false;
 
-        // Initialize destinations
-        destinations = new HashMap<Integer, ArrayList<Integer>>();
+		// Initialize destinations
+		destinations = new HashMap<Integer, ArrayList<Integer>>();
 
-        // Call nextLine() to load the first instruction
-        this.nextLine();
-    }
-    
-    public FileLoader(String fileName, boolean isInstruction) throws Exception{
-    	instructionFile = new File("src/test/"+fileName);
-        try {
-            scanner = new Scanner(instructionFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            endOfFile = true;
-        }
-        endOfFile = false;
+		// Call nextLine() to load the first instruction
+		this.nextLine();
+	}
 
-        destinations = new HashMap<Integer, ArrayList<Integer>>();
-        // Call nextLine() to load the first instruction
-        if (isInstruction) this.nextLine();
-    }
+	public FileLoader(String fileName, boolean isInstruction) throws Exception{
+		instructionFile = new File("src/test/"+fileName);
+		try {
+			scanner = new Scanner(instructionFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			endOfFile = true;
+		}
+		endOfFile = false;
 
-    public boolean hasNextInstruction(){
-        return !endOfFile && scanner.hasNextLine();
-    }
+		destinations = new HashMap<Integer, ArrayList<Integer>>();
+		// Call nextLine() to load the first instruction
+		if (isInstruction) this.nextLine();
+	}
 
-    // Add curLine instruction into destinations,
-    // should only be called by nextLine()
-    private void pushDestinations(){
-        if (!destinations.containsKey(departFloor())){
-            // create new arraylist on demand
-            destinations.put(departFloor(), new ArrayList<Integer>());
-        }
-        // add to arraylist
-        destinations.get(departFloor()).add(destFloor());
-    }
+	public boolean hasNextInstruction(){
+		return !endOfFile && scanner.hasNextLine();
+	}
 
-    // Get destination floors for a specific floor
-    // should only be called by elevator when the elevator arrived on demand
-    public Integer[] popDestinations(int departFloor, boolean goingUp){
-        if(destinations.containsKey(departFloor)){
-            ArrayList<Integer> destinationFloors = destinations.get(departFloor);
-            ArrayList<Integer> output = new ArrayList<Integer>();
+	// Add curLine instruction into destinations,
+	// should only be called by nextLine()
+	private void pushDestinations(){
+		if (!destinations.containsKey(departFloor())){
+			// create new arraylist on demand
+			destinations.put(departFloor(), new ArrayList<Integer>());
+		}
+		// add to arraylist
+		destinations.get(departFloor()).add(destFloor());
+	}
 
-            for (int destination: destinationFloors){
-                if((goingUp && destination > departFloor) ||
-                  (!goingUp && destination < departFloor)){
-                    // Add destination to output
-                    output.add(destination);
-                }
-            }
+	// Get destination floors for a specific floor
+	// should only be called by elevator when the elevator arrived on demand
+	public Integer[] popDestinations(int departFloor, boolean goingUp){
+		if(destinations.containsKey(departFloor)){
+			ArrayList<Integer> destinationFloors = destinations.get(departFloor);
+			ArrayList<Integer> output = new ArrayList<Integer>();
 
-            for (int destination: output){
-                // Remove destination from destinationFloors
-                destinationFloors.remove(Integer.valueOf(destination));
-            }
+			for (int destination: destinationFloors){
+				if((goingUp && destination > departFloor) ||
+						(!goingUp && destination < departFloor)){
+					// Add destination to output
+					output.add(destination);
+				}
+			}
 
-            // if destinationFloors is empty, cleanup
-            if (destinationFloors.size() == 0){
-                destinations.remove(departFloor);
-            }
+			for (int destination: output){
+				// Remove destination from destinationFloors
+				destinationFloors.remove(Integer.valueOf(destination));
+			}
 
-            return output.toArray(new Integer[0]);
-        }
-        // return empty int[] if no destination.
-        return new Integer[0];
-    }
+			// if destinationFloors is empty, cleanup
+			if (destinationFloors.size() == 0){
+				destinations.remove(departFloor);
+			}
 
-    // Returns a new line of the instruction file
-    private String readLine(){
-        return endOfFile ? "" : scanner.nextLine();
-    }
+			return output.toArray(new Integer[0]);
+		}
+		// return empty int[] if no destination.
+		return new Integer[0];
+	}
 
-    // Public method to switch to next instruction
-    public boolean nextLine() throws Exception {
-        if (hasNextInstruction()){
-            curLine = readLine();
-            if(! curLine.equals("")){
-                // Split line into 4 segments: Time, Departure floor, Direction, Destination floor
-                lineSplit = curLine.split(" ");
-                if (lineSplit.length != 4){
-                    throw new Exception("Instruction File format unsupported!");
-                }
-                // Update destinations
-                pushDestinations();
-                return true;
-            }
-        }
-        return false;
-    }
+	// Returns a new line of the instruction file
+	private String readLine(){
+		return endOfFile ? "" : scanner.nextLine();
+	}
 
-    public boolean nextLineErr() throws Exception {
-        if (hasNextInstruction()){
-            curLine = readLine();
-            if(! curLine.equals("")){
-                // Split line into 4 segments: Time, Departure floor, Direction, Destination floor
-                lineSplit = curLine.split(" ");
-                if (lineSplit.length != 3){
-                    throw new Exception("Error File format unsupported!");
-                }
-                return true;
-            }
-        }
-        return false;
-    }
+	// Public method to switch to next instruction
+	public boolean nextLine() throws Exception {
+		if (hasNextInstruction()){
+			curLine = readLine();
+			if(! curLine.equals("")){
+				// Split line into 4 segments: Time, Departure floor, Direction, Destination floor
+				lineSplit = curLine.split(" ");
+				if (lineSplit.length != 3 || lineSplit.length != 4){
+					throw new Exception("Instruction File format unsupported!");
+				}
+				else if (lineSplit.length == 4) {
+					// Update destinations
+					pushDestinations();
+					return true;
+				}
+				else if (lineSplit.length == 3) {
+					
+				}
+			}
+		}
+		return false;
+	}
 
-    // Public method to get time of current instruction
-    public LocalTime getTime(){
-        return LocalTime.parse(lineSplit[0], DateTimeFormatter.ISO_TIME);
-    }
+	public boolean nextLineErr() throws Exception {
+		if (hasNextInstruction()){
+			curLine = readLine();
+			if(! curLine.equals("")){
+				// Split line into 4 segments: Time, Departure floor, Direction, Destination floor
+				lineSplit = curLine.split(" ");
+				if (lineSplit.length != 3){
+					throw new Exception("Error File format unsupported!");
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 
-    // Public method to get departure floor of current instruction
-    public int departFloor(){
-        int result = 0;
-        try{
-            result = Integer.parseInt(lineSplit[1]);
-        }catch(Exception e){
-            System.out.println("ERROR: Parse instruction failed!");
-        }
-        return result;
-    }
+	// Public method to get time of current instruction
+	public LocalTime getTime(){
+		return LocalTime.parse(lineSplit[0], DateTimeFormatter.ISO_TIME);
+	}
 
-    // Public method to get request direction of current instruction
-    public boolean requestUp() {
-        return lineSplit[2].equals("Up");
-    }
+	// Public method to get departure floor of current instruction
+	public int departFloor(){
+		int result = 0;
+		try{
+			result = Integer.parseInt(lineSplit[1]);
+		}catch(Exception e){
+			System.out.println("ERROR: Parse instruction failed!");
+		}
+		return result;
+	}
 
-    // Public method to get destination floor of current instruction
-    public int destFloor(){
-        int result = 0;
-        try{
-            result = Integer.parseInt(lineSplit[3]);
-        }catch(Exception e){
-            System.out.println("ERROR: Parse instruction failed!");
-        }
-        return result;
-    }
-    
-    // Method to read the generated errors for elevators
-    public static String errorFileReader (int elevNum) throws FileNotFoundException {
-    	String output = null;
-    	File errorFile = new File ("src/test/errorFile.txt");   //file location and name
-    	Boolean errorFound = false;  //boolean status of file search
-    	
+	// Public method to get request direction of current instruction
+	public boolean requestUp() {
+		return lineSplit[2].equals("Up");
+	}
+
+	// Public method to get destination floor of current instruction
+	public int destFloor(){
+		int result = 0;
+		try{
+			result = Integer.parseInt(lineSplit[3]);
+		}catch(Exception e){
+			System.out.println("ERROR: Parse instruction failed!");
+		}
+		return result;
+	}
+
+	// Method to read the generated errors for elevators
+	public static String errorFileReader (int elevNum) throws FileNotFoundException {
+		String output = null;
+		File errorFile = new File ("src/test/errorFile.txt");   //file location and name
+		Boolean errorFound = false;  //boolean status of file search
+
 		Scanner scanner = new Scanner (errorFile);
 
 		while (scanner.hasNextLine() && errorFound == false) {  //while elevator has not been found
-        	//output = null;
-    		String line = scanner.nextLine();
-    		String[] currentLine = line.split(" ");   //stores each column in separate index of this string[]
-    		
-    		int num = Integer.parseInt(currentLine[1]);  //store the file's elevator number
-    		
-    		if (num == elevNum) {  //if the current line contains the same elevNum as requested
-    			errorFound = true;    // possible error has been found
-    			
-    			if (LocalTime.now().isAfter(LocalTime.parse(currentLine[0]))) {   // if error time has past
-    				output = currentLine[2];    // set up output to be the error code
-    			}
-    			else {
-    				output = null;
-    			}
-    		}
-    		
-    	}
+			//output = null;
+			String line = scanner.nextLine();
+			String[] currentLine = line.split(" ");   //stores each column in separate index of this string[]
+
+			if (currentLine.length == 3) {
+
+				int num = Integer.parseInt(currentLine[1]);  //store the file's elevator number
+
+				if (num == elevNum) {  //if the current line contains the same elevNum as requested
+					errorFound = true;    // possible error has been found
+
+					if (LocalTime.now().isAfter(LocalTime.parse(currentLine[0]))) {   // if error time has past
+						output = currentLine[2];    // set up output to be the error code
+					}
+					else {
+						output = null;
+					}
+				}
+
+			}
+
+		}
 		scanner.close();
-    	return output;  // return possible error code
-    }
+		return output;  // return possible error code
+	}
 
-    // toString() override
-    public String toString(){
-        // print current line information
-        return "User at floor " + departFloor() + " requested to move " +
-                (requestUp() ? " UP " : "DOWN") + " to floor " +
-                destFloor() + " @ " + getTime();
-    }
+	// toString() override
+	public String toString(){
+		// print current line information
+		return "User at floor " + departFloor() + " requested to move " +
+		(requestUp() ? " UP " : "DOWN") + " to floor " +
+		destFloor() + " @ " + getTime();
+	}
 
 
-    public static void main(String[] args) throws Exception {
-        FileLoader fileLoader = new FileLoader();
+	public static void main(String[] args) throws Exception {
+		FileLoader fileLoader = new FileLoader();
 
-        fileLoader.nextLine();
+		fileLoader.nextLine();
 
-        Integer[] destinations = fileLoader.popDestinations(19, false);
+		Integer[] destinations = fileLoader.popDestinations(19, false);
 
-        for (int destination: destinations) {
-            System.out.println(destination);
-        }
-    }
+		for (int destination: destinations) {
+			System.out.println(destination);
+		}
+	}
 
 }
